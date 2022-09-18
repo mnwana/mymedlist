a0a0c3c8d0d57216fffeaac90e9dad44dcfc2400
mnwana <mariellenwana@gmail.com>
2022-09-18 16:07:21 -0400

    add homepage files

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const sess = {
 
 const hbs = exphbs.create();
 
-// app.engine('handlebars',hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars',hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
