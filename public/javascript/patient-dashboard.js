f4dfb955100f601bdeb81bdf5ae8562b25ad4001
mnwana <mariellenwana@gmail.com>
2022-09-21 20:15:37 -0400

    add save btn handler

--- public/javascript/patient-dashboard.js
+++ public/javascript/patient-dashboard.js
@@ -4,3 +4,28 @@ async function prevLists(event) {
   };
   document.querySelector('#prevLists').addEventListener('click', prevLists);
   
+  async function saveList(event) {
+    event.preventDefault();
+    
+    const list_title = document.querySelector('input[name="list-title"]').value;
+    const list_text = document.querySelector('textarea[name="list-text"]').value;
+    console.log("clicked")
+    const response = await fetch(`/api/lists`, {
+      method: 'POST',
+      body: JSON.stringify({
+        list_title,
+        list_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/patient');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#saveList').addEventListener('click', saveList);
